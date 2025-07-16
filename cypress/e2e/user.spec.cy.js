f7e636c42c07ea1a20588a87867437599324f4c3
ericksonddd <82669984+ericksonddd@users.noreply.github.com>
2025-07-16 09:33:58 -0300

    added update

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,53 +1,53 @@
-import userData from '../fixtures/users/userData.json'
+import userData from '../users/userData.json'
+import loginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js'
+import MenuPage from '../pages/menuPage.js'
+
+const lP = new loginPage()
+const Ds = new DashboardPage()
+const Mp = new MenuPage()
 describe('Orange HRM tests', () => {
 
-const selectoresList = {
-  usernameField: "[name='username']",
-  passwordFiend: "[name='password']",
-  loginNutton: "[type= 'submit']",
-  sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-  deshboardGrid: ".orangehrm-dashboard-grid",
-  wrongCredentialAlert: "[role='alert']",
-  myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+const selectorsList = {
+
   firstNameField: "[name='firstName']",
   lastNameField: "[name='lastName']",
   genericField: ".oxd-input--active",
   dateCloseButton: ".--close",
   submitButton: "[type='submit']",
-  genericCombobox: ".oxd-select-text-input"
+  genericComboBox: ".oxd-select-text-input",
 
 }
 
 
-
-
   it.only('User Info Update Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectoresList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectoresList.passwordFiend).type(userData.userSuccess.password)
-    cy.get(selectoresList.loginNutton).click()    
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectoresList.deshboardGrid)
-    cy.get(selectoresList.myInfoButton).click()
-    cy.get(selectoresList.firstNameField).clear().type('FirstNameTest')
-    cy.get(selectoresList.lastNameField).clear().type('lastNameTest')
-    cy.get(selectoresList.genericField).eq(3).clear().type('NickTeste')
-    cy.get(selectoresList.genericField).eq(4).clear().type('Emploid')
-    cy.get(selectoresList.genericField).eq(5).clear().type('Others')
-    cy.get(selectoresList.genericField).eq(6).clear().type('2025-03-10')
-    cy.get(selectoresList.dateCloseButton).click()    
+    lP.accessLoginPage()
+    lP.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+
+    Ds.checkDashboardPage()
+    Mp.accessMyinfo()
+
+    
+    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorsList.lastNameField).clear().type('lastNameTest')
+    cy.get(selectorsList.genericField).eq(3).clear().type('NickTeste')
+    cy.get(selectorsList.genericField).eq(4).clear().type('Emploid')
+    cy.get(selectorsList.genericField).eq(5).clear().type('Others')
+    cy.get(selectorsList.genericField).eq(6).clear().type('2025-03-10')
+    cy.get(selectorsList.dateCloseButton).click()    
     cy.get(':nth-child(5) > :nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon').click()
     cy.get('.oxd-select-dropdown > :nth-child(27)').click()
     cy.get(':nth-child(5) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input').click()
     cy.get('.oxd-select-dropdown > :nth-child(3)').click()
-    cy.get(selectoresList.submitButton).eq(0).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+ 
   })
 
   it('Login Fail', () => {
     cy.visit('/auth/login')
-    cy.get(selectoresList.usernameField).type(userData.userFail.username)
-    cy.get(selectoresList.passwordFiend).type(userData.userFail.password)
-    cy.get(selectoresList.loginNutton).click()
-    cy.get(selectoresList.wrongCredentialAlert)
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
   })
 })
